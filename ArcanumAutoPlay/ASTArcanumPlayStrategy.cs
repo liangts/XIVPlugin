9ab9a2326cb7910ddddb56980ba1a3b445601610
liangts <liangts0423@gmail.com>
2022-05-02 16:41:22 +0800

    stable version 0.1 (#1)
    Refactor using hook
    update job weight

---
+++ ArcanumAutoPlay/ASTArcanumPlayStrategy.cs
@@ -0,0 +1,222 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dalamud.Game.ClientState.Objects.Types;
+using Dalamud.Game.ClientState.Party;
+using FFXIVClientStructs.FFXIV.Client.Game;
+using FFXIVClientStructs.FFXIV.Client.Game.Gauge;
+
+namespace ArcanumAutoPlay;
+
+public class AstrologianArcanumPlayStrategy
+{
+    private static readonly Dictionary<string, float> MeleeJobClassWeights = new Dictionary<string, float>()
+    {
+        { "GLA", 0.0f }, { "PLD", 0.7f },
+        { "MRD", 0.0f }, { "WAR", 0.7f },
+        { "DRK", 0.7f },
+        { "GNB", 0.7f },
+        { "PGL", 1.0f }, { "MNK", 1.0f },
+        { "LNC", 1.0f }, { "DRG", 1.0f },
+        { "ROG", 1.0f }, { "NIN", 1.0f },
+        { "SAM", 1.0f },
+        { "RPR", 1.0f }
+    };
+
+    private static bool IsMeleeArcanum(AstrologianCard arcanum) => arcanum switch
+    {
+        AstrologianCard.Balance or AstrologianCard.Arrow or AstrologianCard.Spear => true,
+        _ => false
+    };
+
+    private static readonly Dictionary<string, float> RangeJobClassWeights = new Dictionary<string, float>()
+    {
+        { "CNJ", 0.0f }, { "WHM", 0.7f },
+        { "ACN", 1.0f }, { "SCH", 0.7f },
+        { "AST", 0.6f },
+        { "SGE", 0.7f },
+        { "ARC", 0.7f }, { "BRD", 0.7f },
+        { "MCH", 0.9f },
+        { "DNC", 0.8f },
+        { "THM", 0.7f }, { "BLM", 1.1f },
+        { "SMN", 1.0f },
+        { "RDM", 1.0f }, {"BLU", 1.0f},
+    };
+
+    private static bool IsRangeArcanum(AstrologianCard arcanum) => arcanum switch
+    {
+        AstrologianCard.Bole or AstrologianCard.Ewer or AstrologianCard.Spire => true,
+        _ => false
+    };
+
+    private static readonly Dictionary<AstrologianCard, uint> ArcanumStatuses = new Dictionary<AstrologianCard, uint>()
+    {
+        { AstrologianCard.Balance, 1882 }, // Melee
+        { AstrologianCard.Bole, 1883 }, // Range
+        { AstrologianCard.Spear, 1885 }, // Melee
+        { AstrologianCard.Spire, 1887 }, // Range
+        { AstrologianCard.Ewer, 1886 }, // Range
+        { AstrologianCard.Arrow, 1884 } // Melee
+    };
+
+    private static bool HasAstrologianStatus(PartyMember partyMember) =>
+        partyMember.Statuses
+            .Select(status =>
+            {
+                if (status.RemainingTime <= 2.5) return false;
+                return status.StatusId switch
+                {
+                    1882 or 1883 or 1884 or 1885 or 1886 or 1887 => true,
+                    _ => false
+                };
+            })
+            .Any(isAstrologianStatus => isAstrologianStatus);
+
+    private static bool HasCorrectAstrologianStatus(PartyMember partyMember)
+    {
+        var jobAbbr = partyMember.ClassJob.GameData!.Abbreviation.ToString().ToUpper();
+
+        if (MeleeJobClassWeights.ContainsKey(jobAbbr))
+        {
+            return partyMember.Statuses.Select(status =>
+            {
+                if (status.RemainingTime <= 2.5) return false;
+                return status.StatusId switch
+                {
+                    1882 or 1884 or 1885 => true,
+                    _ => false
+                };
+            }).Any(isAstrologianStatus => isAstrologianStatus);
+        }
+
+        if (RangeJobClassWeights.ContainsKey(jobAbbr))
+        {
+            return partyMember.Statuses.Select(status =>
+            {
+                if (status.RemainingTime <= 2.5) return false;
+                return status.StatusId switch
+                {
+                    1883 or 1886 or 1887 => true,
+                    _ => false
+                };
+            }).Any(isAstrologianStatus => isAstrologianStatus);
+        }
+
+        throw new InvalidOperationException("unknown party member job");
+    }
+
+    private static bool HasWeakness(PartyMember partyMember) =>
+        partyMember.Statuses.Select(status =>
+        {
+            if (status.RemainingTime <= 3) return false;
+            if (ConstantsStatusId.StatusId("Ë¥Èõ").Any(id => status.StatusId == id)) return true;
+            if (ConstantsStatusId.StatusId("Weakness").Any(id => status.StatusId == id)) return true;
+            if (ConstantsStatusId.StatusId("Brink of Death").Any(id => status.StatusId == id)) return true;
+            if (ConstantsStatusId.StatusId("Ë¥Èõ£ÛŠ£Ý").Any(id => status.StatusId == id)) return true;
+            return false;
+        }).Any(isWeakNess => isWeakNess);
+
+    private static float ApplyDebuffCoefficient(PartyMember partyMember) =>
+        partyMember.Statuses.Select(status =>
+        {
+            float coef = 1.0f;
+            if (status.RemainingTime <= 3) return coef;
+            //if (ConstantsStatusId.StatusId("Ë¥Èõ").Any(id => status.StatusId == id)) coef = coef * 0.75f;
+            if (ConstantsStatusId.StatusId("Weakness").Any(id => status.StatusId == id)) coef = coef * 0.75f;
+            if (ConstantsStatusId.StatusId("Brink of Death").Any(id => status.StatusId == id)) coef = coef * 0.5f;
+            //if (ConstantsStatusId.StatusId("Ë¥Èõ£ÛŠ£Ý").Any(id => status.StatusId == id)) coef = coef * 0.5f;
+            return coef;
+        }).Aggregate(1.0f, (x, y) => x * y);
+
+    private static float ApplyBuffCoefficient(PartyMember partyMember) =>
+        partyMember.Statuses.Select(status =>
+        {
+            float coef = 1.0f;
+            if (status.RemainingTime <= 3) return coef;
+            if (ConstantsStatusId.StatusId("¥¯¥í©`¥º¥É¥Ý¥¸¥·¥ç¥ó£Û±»£Ý").Any(id => status.StatusId == id)) coef = coef * 1.1f;
+            return coef;
+        }).Aggregate(1.0f, (x, y) => x * y);
+
+    private static readonly Random Random = new Random();
+
+    private static GameObject FindBestCandidate(AstrologianCard arcanum, uint strictLevel = 3)
+    {
+        if (Services.PartyList.Length == 0)
+        {
+            /* Solo Party */
+            return Services.ClientState.LocalPlayer!;
+        }
+
+        var candidates = new List<KeyValuePair<PartyMember, float>>(8);
+
+        if (strictLevel == 0)
+        {
+            /* We are losing hope on choosing the best target, just pick anyone randomly */
+            return Services.PartyList[Random.Next(Services.PartyList.Length)]!.GameObject!;
+        }
+
+        foreach (var partyMember in Services.PartyList)
+        {
+            if (partyMember.GameObject == null) continue;
+            var jobAbbr = partyMember.ClassJob.GameData!.Abbreviation.ToString().ToUpper();
+
+            if (strictLevel == 3)
+            {
+                /* Ignore players that already have an arcanum */
+                if (HasAstrologianStatus(partyMember)) continue;
+            }
+            if (strictLevel == 2)
+            {
+                /* Ignore players that already have an 'correct' arcanum */
+                if (HasCorrectAstrologianStatus(partyMember)) continue;
+            }
+            if (MeleeJobClassWeights.ContainsKey(jobAbbr) && IsMeleeArcanum(arcanum))
+            {
+                float rank = MeleeJobClassWeights[jobAbbr];
+                rank = rank * ApplyBuffCoefficient(partyMember) * ApplyDebuffCoefficient(partyMember);
+                candidates.Add(new KeyValuePair<PartyMember, float>(partyMember, rank));
+            }
+
+            if (RangeJobClassWeights.ContainsKey(jobAbbr) && IsRangeArcanum(arcanum))
+            {
+                float rank = RangeJobClassWeights[jobAbbr];
+                rank = rank * ApplyBuffCoefficient(partyMember) * ApplyDebuffCoefficient(partyMember);
+                candidates.Add(new KeyValuePair<PartyMember, float>(partyMember, rank));
+            }
+        }
+
+        if (candidates.Count == 0)
+        {
+            return FindBestCandidate(arcanum, strictLevel - 1);
+        }
+
+        candidates.Sort((lhs, rhs) => lhs.Value.CompareTo(rhs.Value));
+        candidates.Reverse();
+
+        var bestRank = candidates[0].Value;
+        candidates = candidates.FindAll(candidate => candidate.Value == bestRank);
+        Services.ChatGui.Print("Best Rank: " + bestRank.ToString());
+
+        var chosenOne = candidates[Random.Next(candidates.Count)].Key;
+
+        return chosenOne.GameObject!;
+    }
+
+    private static unsafe AstrologianCard GetCurrentDrewCard() =>
+        JobGaugeManager.Instance()->Astrologian.CurrentCard;
+
+    private static unsafe AstrologianSeal[] GetCurrentSeals() =>
+        JobGaugeManager.Instance()->Astrologian.CurrentSeals;
+
+    public GameObject SelectTarget()
+    {
+        if (Services.ClientState.LocalPlayer!.ClassJob.GameData!.Abbreviation.ToString() != "AST")
+            throw new InvalidOperationException("job of the character must be astrologian");
+
+        var arcanumDrew = GetCurrentDrewCard();
+        if (arcanumDrew == AstrologianCard.None)
+            throw new InvalidOperationException("an arcanum must be drew before selects target");
+
+        return FindBestCandidate(arcanumDrew);
+    }
+}
--- ArcanumAutoPlay/Plugin.cs
+++ ArcanumAutoPlay/Plugin.cs
@@ -1,256 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using Dalamud.Game.ClientState;
-using Dalamud.Game.ClientState.Objects.Types;
-using Dalamud.Game.ClientState.Party;
-using Dalamud.Game.Command;
-using Dalamud.Game.Gui;
+using Dalamud.Hooking;
 using Dalamud.IoC;
 using Dalamud.Plugin;
 using FFXIVClientStructs.FFXIV.Client.Game;
+using FFXIVClientStructs.FFXIV.Client.Game.Gauge;
 
-namespace ArcanumDrawer;
+namespace ArcanumAutoPlay;
 
-public class Plugin : IDalamudPlugin
+public unsafe class Plugin : IDalamudPlugin
 {
     public string Name => "ArcanumAutoPlay";
 
-    private ClientState ClientState { get; }
-    private ChatGui ChatGui { get; }
-    private CommandManager CommandManager { get; }
-    private PartyList PartyList { get; }
+    private delegate byte UseActionDelegate(ActionManager* actionManager, ActionType actionType, uint actionId,
+        long targetId, uint a4, uint a5, uint a6, void* a7);
 
-    public Plugin(
-        [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
-        ClientState clientState,
-        ChatGui chatGui,
-        CommandManager commandManager,
-        PartyList partyList)
+    private static Hook<UseActionDelegate> UseActionHook;
+
+    private static byte UseActionDetour(ActionManager* actionManager, ActionType actionType, uint actionId,
+        long targetId, uint a4, uint a5, uint a6, void* a7)
     {
-        ClientState = clientState;
-        ChatGui = chatGui;
-        CommandManager = commandManager;
-        PartyList = partyList;
-
-        CommandManager.AddHandler("/parcanumautoplay", new CommandInfo(OnCommandArcanumAutoPlay)
+        if (actionId != 17055)
         {
-            HelpMessage = "Dummy Help Message"
-        });
+            return UseActionHook.Original(actionManager, actionType, actionId, targetId, a4, a5, a6, a7);
+        }
 
+        if (JobGaugeManager.Instance()->Astrologian.CurrentCard is
+            AstrologianCard.None or AstrologianCard.Lady or AstrologianCard.Lord)
+        {
+            Services.ChatGui.Print($"[[Arcanum] Direct Pass");
+            return UseActionHook.Original(actionManager, actionType, actionId, targetId, a4, a5, a6, a7);
+        }
+
+        var strategy = new AstrologianArcanumPlayStrategy();
+        var target = strategy.SelectTarget();
+
+        Services.ChatGui.Print($"[Arcanum] {JobGaugeManager.Instance()->Astrologian.CurrentCard} -> {target}");
+        return UseActionHook.Original(actionManager, actionType, actionId, target.ObjectId, a4, a5, a6, a7);
+    }
+
+    public Plugin([RequiredVersion("1.0")] DalamudPluginInterface pluginInterface)
+    {
+        pluginInterface.Create<Services>();
         pluginInterface.UiBuilder.Draw += () => { };
         pluginInterface.UiBuilder.OpenConfigUi += () => { };
+
+        UseActionHook = new Hook<UseActionDelegate>((IntPtr)ActionManager.fpUseAction, UseActionDetour);
+        UseActionHook.Enable();
     }
 
     public void Dispose()
     {
-        CommandManager.RemoveHandler("/parcanumautoplay");
-    }
-
-    private enum Arcanum
-    {
-        Balance,
-        Arrow,
-        Spear,
-        Bole,
-        Ewer,
-        Spire
-    }
-
-    private static readonly Dictionary<string, uint> MeleeJobClassWeights = new Dictionary<string, uint>()
-    {
-        { "GLA", 0 }, { "PLD", 2 },
-        { "MRD", 0 }, { "WAR", 2 },
-        { "DRK", 2 },
-        { "GNB", 2 },
-        { "PGL", 1 }, { "MNK", 4 },
-        { "LNC", 1 }, { "DRG", 3 },
-        { "ROG", 1 }, { "NIN", 3 },
-        { "SAM", 4 },
-        { "RPR", 4 }
-    };
-
-    private static bool IsMeleeArcanum(Arcanum arcanum) => arcanum switch
-    {
-        Arcanum.Balance or Arcanum.Arrow or Arcanum.Spear => true,
-        _ => false
-    };
-
-
-    private static readonly Dictionary<string, uint> RangeJobClassWeights = new Dictionary<string, uint>()
-    {
-        { "CNJ", 0 }, { "WHM", 2 },
-        { "ACN", 1 }, { "SCH", 2 },
-        { "AST", 2 },
-        { "SGE", 2 },
-        { "ARC", 1 }, { "BRD", 3 },
-        { "MCH", 3 },
-        { "DNC", 3 },
-        { "THM", 1 }, { "BLM", 4 },
-        { "SMN", 4 },
-        { "RDM", 4 },
-    };
-
-    private static bool IsRangeArcanum(Arcanum arcanum) => arcanum switch
-    {
-        Arcanum.Bole or Arcanum.Ewer or Arcanum.Spire => true,
-        _ => false
-    };
-
-    private bool HasArcanumDrew()
-    {
-        foreach (var status in ClientState.LocalPlayer!.StatusList)
+        if (UseActionHook.IsEnabled)
         {
-            if (status.StatusId == 2713) // Status: Clarifying Draw
-            {
-                return true;
-            }
+            UseActionHook.Disable();
         }
-
-        return false;
-    }
-
-    private static readonly Dictionary<Arcanum, uint> PlayActionId = new Dictionary<Arcanum, uint>()
-    {
-        { Arcanum.Balance, 4401 },
-        { Arcanum.Arrow, 4402 },
-        { Arcanum.Spear, 4403 },
-        { Arcanum.Bole, 4404 },
-        { Arcanum.Ewer, 4405 },
-        { Arcanum.Spire, 4406 }
-    };
-
-    private static readonly Random Random = new Random();
-    private static bool _workerThreadOnHold;
-
-    private unsafe void ArcanumAutoPlay()
-    {
-        if (_workerThreadOnHold) return;
-        _workerThreadOnHold = true;
-        try
-        {
-            var actionManager = ActionManager.Instance();
-            var localPlayer = ClientState.LocalPlayer!.ObjectId;
-
-            const uint actionIdDraw = 3590;
-            const ActionType actionTypeDraw = ActionType.Spell;
-
-            if (!HasArcanumDrew())
-            {
-                if (actionManager->GetActionStatus(actionTypeDraw, actionIdDraw, localPlayer, 0, 0) != 0)
-                {
-                    return;
-                }
-
-                var gcd = actionManager->GetRecastTime(ActionType.Spell, 3599);
-                var gcdElapsed = actionManager->GetRecastTimeElapsed(ActionType.Spell, 3599);
-
-                while ((gcd != 0) && (((gcdElapsed <= 1) || (gcd - gcdElapsed <= 1))))
-                {
-                    Thread.Yield();
-                }
-
-                ChatGui.Print("here");
-                
-                if (!actionManager->UseAction(actionTypeDraw, actionIdDraw, ClientState.LocalPlayer!.ObjectId))
-                {
-                    ChatGui.Print("failed");
-                    return;
-                }
-                
-                ChatGui.Print("success");
-            }
-
-            var arcanum = Arcanum.Balance;
-
-            while (!HasArcanumDrew())
-            {
-                Thread.Sleep(1000);
-                Thread.Yield();
-            }
-
-            foreach (var status in ClientState.LocalPlayer!.StatusList)
-            {
-                arcanum = status.StatusId switch
-                {
-                    913 => Arcanum.Balance,
-                    915 => Arcanum.Arrow,
-                    916 => Arcanum.Spear,
-                    914 => Arcanum.Bole,
-                    917 => Arcanum.Ewer,
-                    918 => Arcanum.Spire,
-                    _ => arcanum
-                };
-            }
-
-            var target = FindBestCandidate(arcanum);
-            var actionIdPlay = PlayActionId[arcanum];
-            const ActionType actionTypePlay = ActionType.Spell;
-
-            ChatGui.Print($"[Arcanum] {arcanum} -> {target}");
-
-            var gcd2 = actionManager->GetRecastTime(ActionType.Spell, 3599);
-            var gcdElapsed2 = actionManager->GetRecastTimeElapsed(ActionType.Spell, 3599);
-
-            while ((gcd2 != 0) && (((gcdElapsed2 <= 1) || (gcd2 - gcdElapsed2 <= 1))))
-            {
-                Thread.Yield();
-            }
-
-            actionManager->UseAction(actionTypePlay, actionIdPlay, target.ObjectId);
-        }
-        catch (Exception exception)
-        {
-            ChatGui.Print(exception.Message);
-        }
-        finally
-        {
-            _workerThreadOnHold = false;
-        }
-    }
-
-
-    private GameObject FindBestCandidate(Arcanum arcanum)
-    {
-        var candidates = new List<KeyValuePair<PartyMember, uint>>(8);
-
-        foreach (var partyMember in PartyList)
-        {
-            var jobAbbr = partyMember.ClassJob.GameData!.Abbreviation.ToString().ToUpper();
-
-            if (MeleeJobClassWeights.ContainsKey(jobAbbr) && IsMeleeArcanum(arcanum))
-            {
-                var rank = MeleeJobClassWeights[jobAbbr];
-                candidates.Add(new KeyValuePair<PartyMember, uint>(partyMember, rank));
-            }
-
-            if (RangeJobClassWeights.ContainsKey(jobAbbr) && IsRangeArcanum(arcanum))
-            {
-                var rank = RangeJobClassWeights[jobAbbr];
-                candidates.Add(new KeyValuePair<PartyMember, uint>(partyMember, rank));
-            }
-        }
-
-        if (candidates.Count == 0)
-        {
-            return ClientState.LocalPlayer!;
-        }
-
-        candidates.Sort((lhs, rhs) => lhs.Value.CompareTo(rhs.Value));
-        candidates.Reverse();
-
-        var bestRank = candidates[0].Value;
-        candidates = candidates.FindAll(candidate => candidate.Value == bestRank);
-
-        var chosenOne = candidates[Random.Next(candidates.Count)].Key;
-
-        return chosenOne.GameObject!;
-    }
-
-
-    private void OnCommandArcanumAutoPlay(string command, string args)
-    {
-        var workerThread = new Thread(ArcanumAutoPlay);
-        workerThread.Start();
     }
 }
